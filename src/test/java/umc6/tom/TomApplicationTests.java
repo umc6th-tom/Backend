1b5938e6a232b4c16ab272e4bc47d711bccceac7
GitHyeon <ryuoo0123@gmail.com>
2024-07-11 00:02:52 +0900

    git init

---
+++ src/test/java/umc6/tom/TomApplicationTests.java
@@ -0,0 +1,13 @@
+package umc6.tom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
