11aea3fc6eb09e8b73d1bf72b3e76f306587d4dc
GitHyeon <ryuoo0123@gmail.com>
2024-07-17 01:38:03 +0900

    Feat : JwtTokenProvider 구현

--- src/main/java/umc6/tom/user/model/User.java
+++ src/main/java/umc6/tom/user/model/User.java
@@ -2,17 +2,22 @@ package umc6.tom.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import umc6.tom.common.BaseEntity;
 import umc6.tom.user.model.enums.Gender;
 import umc6.tom.user.model.enums.Major;
 import umc6.tom.user.model.enums.SocialType;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +32,16 @@ public class User extends BaseEntity {
 
     @Column(name = "password") // nullable = false 추가
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 /*
 
     @Column(name = "pic")
