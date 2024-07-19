3903e04e72302b74a580ed4c46127e408b2d50f3
selloriwoo <sellpriwoo@gmail.com>
2024-07-19 20:42:01 +0900

    Feat: AlarmSet의 entity, enum 추가, entity 매핑 관계 수정

--- src/main/java/umc6/tom/common/model/Majors.java
+++ src/main/java/umc6/tom/common/model/Majors.java
@@ -2,6 +2,8 @@ package umc6.tom.common.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import umc6.tom.board.model.Board;
+import umc6.tom.board.model.BoardLike;
 import umc6.tom.common.BaseEntity;
 import umc6.tom.common.model.enums.Major;
 import umc6.tom.user.model.User;
@@ -22,6 +24,9 @@ public class Majors {
 
     @Enumerated(EnumType.STRING)
     private Major major;
+
+    @OneToMany(mappedBy = "majors", cascade = CascadeType.ALL)
+    private List<Board> boardList = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "college_id")
