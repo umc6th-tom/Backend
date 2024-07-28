552031fe5cafa299745a3d24c66d76a6d444c77b
selloriwoo <sellpriwoo@gmail.com>
2024-07-28 18:01:06 +0900

    Feat: 글 작성, 글 수정 사진 API 구현, 일부 변수 수정 및 추가

---
+++ src/main/java/umc6/tom/board/repository/BoardComplaintPictureRepository.java
@@ -0,0 +1,7 @@
+package umc6.tom.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc6.tom.board.model.BoardComplaintPicture;
+
+public interface BoardComplaintPictureRepository extends JpaRepository<BoardComplaintPicture, Long> {
+}
