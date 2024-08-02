24addb6c95bfb045bcace09227aadff987d31177
selloriwoo <sellpriwoo@gmail.com>
2024-08-02 23:40:00 +0900

    Feat: 유저id 변수 설정

--- src/main/java/umc6/tom/alarm/controller/AlarmController.java
+++ src/main/java/umc6/tom/alarm/controller/AlarmController.java
@@ -28,31 +28,31 @@ public class AlarmController {
     @GetMapping("/list/board/{is_read}")
     public ApiResponse<AlarmResponseDto.writtenBoardViewListDto> getWrittenBoardAlarm(@RequestParam(name = "page") Integer page,
                                                                                    @PathVariable(name = "is_read") String isRead){
-        Long userId = 1L;
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(alaramService.getBoardAlarmList(userId, isRead, page));
     }
     @GetMapping("/list/comment/{is_read}")
     public ApiResponse<AlarmResponseDto.writtenBoardViewListDto> getCommentedAlarm(@RequestParam(name = "page") Integer page,
                                                                                       @PathVariable(name = "is_read") String isRead){
-        Long userId = 1L;
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(alaramService.getCommentedList(userId, isRead, page));
     }
     @GetMapping("/list/like/{is_read}")
     public ApiResponse<AlarmResponseDto.writtenBoardViewListDto> getLikedAlarm(@RequestParam(name = "page") Integer page,
                                                                                       @PathVariable(name = "is_read") String isRead){
-        Long userId = 1L;
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(alaramService.getLikedList(userId, isRead, page));
     }
     @GetMapping("/list/all/{is_read}")
     public ApiResponse<AlarmResponseDto.writtenBoardViewListDto> getAllAlarm(@RequestParam(name = "page") Integer page,
                                                                                       @PathVariable(name = "is_read") String isRead){
-        Long userId = 1L;
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(alaramService.getAllAlarmList(userId, isRead, page));
     }
 
     @PatchMapping("/list/read")
     public ApiResponse<AlarmResponseDto.updateReadAlarmDto> updateAlarmRead(@RequestBody AlarmRequestDto.ReadAlarmDto request){
-        Long userId = 1L;
+        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(alaramService.updateReadAlarm(userId, request));
     }
 }
