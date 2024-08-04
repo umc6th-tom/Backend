8f5a3512eea8959a1ec8bc8b9ebf6023c25ea5e9
selloriwoo <sellpriwoo@gmail.com>
2024-08-04 23:02:37 +0900

    feature: 게시글 수정, 게시물 신고 로직 변경

--- src/main/java/umc6/tom/board/model/enums/BoardStatus.java
+++ src/main/java/umc6/tom/board/model/enums/BoardStatus.java
@@ -2,5 +2,5 @@ package umc6.tom.board.model.enums;
 
 public enum BoardStatus {
     //활성화, 신고(1번이라도 된 상태), 신고상태에서 삭제(데이터는 남아있고 화면에 안보임), 일정 횟수이상 신고시 안보이게
-    ACTIVE, COMPLAINT, COMPLAINTDELETE, OVERCOMPLAINT
+    ACTIVE, COMPLAINTDELETE, OVERCOMPLAINT
 }
