d783fedc04d7123b3e75b52aed750372bce9d8c0
selloriwoo <sellpriwoo@gmail.com>
2024-08-09 18:54:14 +0900

    Feat: 자주 묻는 질문 제목, 내용 둘다 검색되게 변경 및 QNA의 불 필요한 코드 제거, 수정

--- src/main/java/umc6/tom/fAQ/repository/FAQRepository.java
+++ src/main/java/umc6/tom/fAQ/repository/FAQRepository.java
@@ -3,16 +3,20 @@ package umc6.tom.fAQ.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import umc6.tom.fAQ.model.FAQ;
 import umc6.tom.fAQ.model.enums.Category;
 
+@Repository
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
     Page<FAQ> findAllByOrderByUpdatedAtDesc(PageRequest pageRequest);
     Page<FAQ> findAllByCategoryOrderByUpdatedAtDesc(Category category, PageRequest pageRequest);
 
-    Page<FAQ> findAllByTitleContainingOrderByUpdatedAtDesc(String title, PageRequest pageRequest);
-    Page<FAQ> findAllByCategoryAndTitleContainingOrderByUpdatedAtDesc
-            (Category category, String title,PageRequest pageRequest);
+    Page<FAQ> findAllByTitleContainingOrContentContainingOrderByUpdatedAtDesc(String title, String content, PageRequest pageRequest);
+
+    @Query("SELECT faq FROM FAQ faq WHERE faq.category = :category AND (faq.title LIKE %:title% OR faq.content LIKE %:content%) ORDER BY faq.createdAt DESC")
+    Page<FAQ> findAllByCategoryAndTitleContainingOrContentContainingOrderByUpdatedAtDesc(Category category,String title, String content, PageRequest pageRequest);
 
 
 }
