48ca45425921448fa91ce3502a6b2e56e41ea90a
selloriwoo <sellpriwoo@gmail.com>
2024-08-10 23:56:22 +0900

    Feat: 게시물 수정 예외 사항 추가 처리, 게시물 신고 response 명칭 수정

--- src/main/java/umc6/tom/board/dto/BoardResponseDto.java
+++ src/main/java/umc6/tom/board/dto/BoardResponseDto.java
@@ -66,7 +66,7 @@ public class BoardResponseDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class BoardComplaintDto{
-        Long boardId;
+        Long boardComplaintId;
         Long userId;
     }
 
