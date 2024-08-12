6b826d7667abdd9056a02bc0cfc90abe22cf404c
qbnyp <moneycloudy@naver.com>
2024-08-13 05:21:08 +0900

    [FEAT] 관리자 - 유저 신고된 글 조회 수정

--- src/main/java/umc6/tom/user/controller/RootUserController.java
+++ src/main/java/umc6/tom/user/controller/RootUserController.java
@@ -138,7 +138,7 @@ public class RootUserController {
      * 24.08.13 작성자 : 서정호
      * 관리자 - 유저 신고된 글 조회
      */
-    @GetMapping("/board/{complaintId}")
+    @GetMapping("/boardcomplaint/{complaintId}")
     public ApiResponse<UserDtoRes.complaintBoardReasonDto> BoardReportReason(@PathVariable("complaintId") Long complaintId) {
 //        Long userId = jwtTokenProvider.getUserIdFromToken();
         return ApiResponse.onSuccess(rootUserService.boardReportReason(complaintId));
