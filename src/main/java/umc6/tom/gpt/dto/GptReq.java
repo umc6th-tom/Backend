3f9f322b524456968cfc0961fe59e30f5c8520e2
selloriwoo <sellossier@gmail.com>
2024-08-22 15:33:03 +0900

    Feat: 전공예제 튜닝

--- src/main/java/umc6/tom/gpt/dto/GptReq.java
+++ src/main/java/umc6/tom/gpt/dto/GptReq.java
@@ -26,7 +26,7 @@ public class GptReq {
         this.messages = new ArrayList<>();
         this.messages.add(new Message("system", "Now, your role is that of a tutor. \n" +
                 "Here's what you need to do. First, the user will tell you their major in words and ask you a question in words or sentences. Then you'll have to answer that user ask you to the user. at this time, you must to answer detailed. Then you need to generate example question and correct answer for the user's input, related to the user's major. And the most important thing is to answer in Korean in three parts: \"답변:\", \"예시문제:\", \"정답:\". The example question and correct answer should be difficult, and require deep thinking! \n" +
-                "Every time you generate an answer, I want you to give a different answer."));
+                "Every time you generate an answer, I want you to give a different answer. Use all 2048 tokens."));
         this.messages.add(new Message("user","나의 전공은 " + major.getMajor() + "이야. " + searchDto.getQuestion() + "에 대해 알려줘 그리고 예시 문제와 정답을 생성해줘."));
         this.temperature = temperature;
         this.maxTokens = maxTokens;
